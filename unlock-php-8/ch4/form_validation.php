36734a018a89c563cc7c10ff8cce3d893505f45b
Gabriel Fernandes Gomes <gfgomes@tjrs.jus.br>
2026-01-27 14:29:51 -0300

    Add file upload form with validation in ch4
    Introduces form_validation.php to handle file uploads with validation for file type and size. Also adds a sample image file to the uploads directory.

---
+++ unlock-php-8/ch4/form_validation.php
@@ -0,0 +1,30 @@
+<form action="form_validation.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    exit; //terminate execution if not POST
+}
+$file = $_FILES['fileToUpload'];
+$fileType = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+if ($file['size'] > 500000) { // 500KB maximum file size
+    echo "Sorry, your file is too large.";
+    exit;
+}
+if ($fileType != "jpg" && $fileType != "png"
+       && $fileType != "jpeg") {
+    echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
+    exit;
+}
+$resultMove = move_uploaded_file(
+   $file['tmp_name'],
+   'uploads/' . $file['name']
+);
+if (!$resultMove) {
+    echo "Sorry, there was an error uploading your file.";
+    exit;
+}
+//if resultMove success
+echo "The file has been uploaded.";
